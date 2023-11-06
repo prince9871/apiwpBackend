2fafb794c29bfa286ddefc3d73b16035942f5412
Prince Soni <61613218+prince9871@users.noreply.github.com>
2023-11-06 11:02:12 +0530

    Update router.js

--- routers/router.js
+++ routers/router.js
@@ -2,7 +2,7 @@ const Express=require('express')
 const Router=Express.Router()
 const createController=require('../controllers/createController')
 
-Router.get('/api', (req, res) => {
+Router.get('/', (req, res) => {
 
   return res.send({ message: 'hello from api' })
 })
